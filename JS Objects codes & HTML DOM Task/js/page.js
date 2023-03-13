f2c6bc8bf05358fb844cb77754601c7956519693
Hikmat Gulizada <118837780+gulizadegh@users.noreply.github.com>
2023-03-13 18:49:54 +0400

    Add files via upload

---
+++ JS Objects codes & HTML DOM Task/js/page.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("userFirstnametext").onclick = function() {
+    let userFirstname=document.getElementById("userText").value;
+    console.log("Hello",userFirstname);
+}
+
+
